0cc029151be93567fa8706ecde50f587e9736b29
unknown <coolbhushans@gmail.com>
2015-10-05 17:22:55 +0530

    access modifies chaged

--- app/models/TodoList.java
+++ app/models/TodoList.java
@@ -22,70 +22,14 @@ public class TodoList extends Model {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	
-	@Column(name = "todo_label")
-	private String todoLabel;
 	
-	@Column(name = "creation_date")
-	private Date creationDate;
+	public String todoLabel;
+	
+	public Date creationDate;
 	 
-	@Column(name = "due_date")
-	private Date dueDate;
+	public Date dueDate;
 	
-	private boolean status;
-	
-	 
-	 public Long getId() {
-		return id;
-	}
-
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-
-	public String getTodoLabel() {
-		return todoLabel;
-	}
-
-
-	public void setTodoLabel(String todoLabel) {
-		this.todoLabel = todoLabel;
-	}
-
-
-	public Date getCreationDate() {
-		return creationDate;
-	}
-
-
-	public void setCreationDate(Date creationDate) {
-		this.creationDate = creationDate;
-	}
-
-	
-	
-	public Date getDueDate() {
-		return dueDate;
-	}
-
-
-	public void setDueDate(Date dueDate) {
-		this.dueDate = dueDate;
-	}
-	
-
-	public boolean getStatus() {
-		return status;
-	}
-
-
-	public void setStatus(boolean status) {
-		this.status = status;
-	}
-	
-	
-	
+	public boolean status;
 
 	public static Finder<Long,TodoList> find = new Finder<Long, TodoList>(Long.class, TodoList.class);
 }
